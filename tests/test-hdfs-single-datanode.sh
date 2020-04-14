0a35b210f92795ac5a88fe043b96a5293289560b
Carlos Giraldo <cgiraldo@gradiant.org>
2020-04-14 09:50:01 +0200

    added test for single datanode deployment

---
+++ tests/test-hdfs-single-datanode.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+HADOOP_VERSION="${HADOOP_VERSION:-2.7.7}"
+
+HADOOP_VERSION=$HADOOP_VERSION docker-compose -f docker-compose-single-datanode.yml up -d
+
+retry=0
+maxRetries=5
+retryInterval=10
+
+until [ ${retry} -ge ${maxRetries} ]
+do
+	docker run -ti --rm --net tests_hdfs gradiant/hdfs:$HADOOP_VERSION \
+  hdfs dfs -test -e hdfs://namenode:8020/ && break
+	echo "Hdfs namenode at hdfs://namenode:8020 not ready"
+  echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
+	sleep ${retryInterval}
+done
+
+docker run -ti --rm --net tests_hdfs -e HDFS_NAMENODE_URL=hdfs://namenode:8020 gradiant/hdfs:$HADOOP_VERSION \
+/bin/bash -c "
+set -ex
+hdfs dfs -ls /
+hdfs dfs -mkdir /test
+hdfs dfs -ls /test
+hdfs dfs -copyFromLocal /entrypoint.sh /test
+hdfs dfs -ls /test/entrypoint.sh
+hdfs dfs -rm /test/entrypoint.sh
+hdfs dfs -rmdir /test
+"
+TEST_RESULT=$?
+
+HADOOP_VERSION=$HADOOP_VERSION docker-compose -f docker-compose-single-datanode.yml down -v
+
+exit $TEST_RESULT
